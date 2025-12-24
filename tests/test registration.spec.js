e8c9ee353e2fccf71997e5df3eae2072f6f7f839
Aizengott <aizengott.mess@gmail.com>
2025-12-24 16:09:50 +0100

    класс пользователя вынесен в отдельный файл

--- tests/test registration.spec.js
+++ tests/test registration.spec.js
@@ -1,25 +1,25 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+import { UserData } from './src/userData';
+//import { faker } from '@faker-js/faker';
 
 const REG_URL = 'https://realworld.qa.guru/';
-const USER_NAME = faker.internet.username();
-const PASSWORD = "Pss!";
-const EMAIL = faker.internet.email({provider: 'example.com'});
 
 test.describe('tests registration and authorization', ()=> { 
 
 test('registration', async ({ page }) => {
+  const user = new UserData().generate();
+  //console.log(user);
   await page.goto(REG_URL);
   await page.getByRole('link', { name: 'Sign up' }).click();
   await expect(page.getByRole('heading', { name: 'Sign up' })).toBeVisible();
   await page.getByRole('textbox', { name: 'Your Name' }).click();
-  await page.getByRole('textbox', { name: 'Your Name' }).fill(USER_NAME);
+  await page.getByRole('textbox', { name: 'Your Name' }).fill(user.userName);
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill(EMAIL);
+  await page.getByRole('textbox', { name: 'Email' }).fill(user.email);
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(PASSWORD);
+  await page.getByRole('textbox', { name: 'Password' }).fill(user.password);
   await page.getByRole('button', { name: 'Sign up' }).click();
-  await expect(page.getByRole('navigation')).toContainText(USER_NAME);
+  await expect(page.getByRole('navigation')).toContainText(user.userName);
 });
 /*test('authorization', async ({ page }) => {
 })*/
